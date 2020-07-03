206303aaf0d776b1355ed54a0b230cddf46cec47
fredrik.lunde@frontedgeit.se <fredrik.lunde@frontedgeit.se>
2020-07-03 21:12:05 +0200

    adding text

--- MMM-GoogleTrafficTimes.js
+++ MMM-GoogleTrafficTimes.js
@@ -1,7 +1,7 @@
-Module.register("MMM-GoogleTrafficTimes",{
+Module.register("MMM-GoogleTrafficTimes", {
 
         // Module config defaults
-        defaults : {
+        defaults: {
                 key: '',
                 origin: 'SW1A 1AA',
                 destination1: 'Work:SW1A 2PW',
@@ -14,22 +14,22 @@ Module.register("MMM-GoogleTrafficTimes",{
                 return ["MMM-GoogleTrafficTimes.css"];
         },
 
-        start: function() {
-        var self = this;
-        Log.info("Starting module: " + this.name);
+        start: function () {
+                var self = this;
+                Log.info("Starting module: " + this.name);
 
-        if (this.config.key === "") {
-                Log.error("MMM-GoogleTrafficTimes: API key not provided or valid!");
-                return;
-        }
+                if (this.config.key === "") {
+                        Log.error("MMM-GoogleTrafficTimes: API key not provided or valid!");
+                        return;
+                }
 
-        setInterval(function() {
-                self.updateDom();
-        }, this.config.updateInterval);
-    },
+                setInterval(function () {
+                        self.updateDom();
+                }, this.config.updateInterval);
+        },
 
-	// Override dom generator.
-	getDom: function() {
+        // Override dom generator.
+        getDom: function () {
                 var origin = this.config.origin;
                 var location1 = this.config.destination1.split(':')[1];
                 var location2 = this.config.destination2.split(':')[1];
@@ -38,59 +38,66 @@ Module.register("MMM-GoogleTrafficTimes",{
                 var name1 = this.config.destination1.split(':')[0];
                 var name2 = this.config.destination2.split(':')[0];
                 var name3 = this.config.destination3.split(':')[0];
-               
-		var re1 = new RegExp(location1, 'g');
+
+                var re1 = new RegExp(location1, 'g');
                 var re2 = new RegExp(location2, 'g');
                 var re3 = new RegExp(location3, 'g');
 
                 var wrapper = document.createElement("div");
-		wrapper.style = "text-align:left;font-size:0.65em;line-height:normal";
+                wrapper.style = "text-align:left;font-size:0.65em;line-height:normal";
 
 
-		var script = document.createElement("script");
+                var script = document.createElement("script");
                 script.type = "text/javascript";
                 script.src = "https://maps.googleapis.com/maps/api/js?key=" + this.config.key;
-                script.setAttribute('defer','');
-                script.setAttribute('async','');
+                script.setAttribute('defer', '');
+                script.setAttribute('async', '');
                 document.body.appendChild(script);
 
                 var self = this;
 
-        script.onload = function () {
-                var geocoder = new google.maps.Geocoder;
-                var service = new google.maps.DistanceMatrixService;
+                script.onload = function () {
+                        var geocoder = new google.maps.Geocoder;
+                        var service = new google.maps.DistanceMatrixService;
 
-        service.getDistanceMatrix({
-          origins: [origin],
-          destinations: [location1, location2, location3],
-          travelMode: 'DRIVING',
-          drivingOptions: {
-                departureTime: new Date(Date.now())
-          },
-          unitSystem: google.maps.UnitSystem.METRIC,
-          avoidHighways: false,
-          avoidTolls: false
-        }, function(response, status) {
-          if (status !== 'OK') {
-            Log.error('Error was: ' + status);
-          } else {
-            var originList = response.originAddresses;
-            var destinationList = response.destinationAddresses;
-            let timings = '';
+                        service.getDistanceMatrix({
+                                origins: [origin],
+                                destinations: [location1, location2, location3],
+                                travelMode: 'DRIVING',
+                                drivingOptions: {
+                                        departureTime: new Date(Date.now())
+                                },
+                                unitSystem: google.maps.UnitSystem.METRIC,
+                                avoidHighways: false,
+                                avoidTolls: false
+                        }, function (response, status) {
+                                if (status !== 'OK') {
+                                        Log.error('Error was: ' + status);
+                                } else {
+                                        var originList = response.originAddresses;
+                                        var destinationList = response.destinationAddresses;
+                                        let timings = '';
 
-            for (var i = 0; i < originList.length; i++) {
-              var results = response.rows[i].elements;
-              for (var j = 0; j < results.length; j++) {
-			var trafficIcon = 'text';
-			if (results[j].duration_in_traffic.value > results[j].duration.value) {trafficIcon = 'textWhenLate'};
-                        if (destinationList[j].match(re1)) {wrapper.innerHTML += '<span class="' + trafficIcon + '"><span>' +  name1 + ' - ' + results[j].duration_in_traffic.text + '</span></span>'};
-                        if (destinationList[j].match(re2)) {wrapper.innerHTML += '<span class="' + trafficIcon + '"><span>' +  name2 + ' - ' + results[j].duration_in_traffic.text + '</span></span>'};
-                        if (destinationList[j].match(re3)) {wrapper.innerHTML += '<span class="' + trafficIcon + '"><span>' +  name3 + ' - ' + results[j].duration_in_traffic.text + '</span></span>'};
-              }
-            }
-          }
-        });
-	}
-		return wrapper;
-	}
+                                        for (var i = 0; i < originList.length; i++) {
+                                                var results = response.rows[i].elements;
+                                                for (var j = 0; j < results.length; j++) {
+                                                        var trafficIcon = 'text';
+                                                        const duration = results[j].duration;
+                                                        const durationInTraffic = results[j].duration_in_traffic;
+                                                        var textWhenLongerThanUsual = "as usual";
+                                                        if (durationInTraffic.value > duration.value) {
+                                                                trafficIcon = 'textWhenLate'
+                                                                const timeLongerThanUsual = duration.value - durationInTraffic.value;
+                                                                textWhenLongerThanUsual = '+' + timeLongerThanUsual + ' minuter'
+                                                        };
+                                                        if (destinationList[j].match(re1)) { wrapper.innerHTML += '<span class="' + trafficIcon + '"><span>' + name1 + ' - ' + durationInTraffic.text + textWhenLongerThanUsual + '</span></span>' };
+                                                        if (destinationList[j].match(re2)) { wrapper.innerHTML += '<span class="' + trafficIcon + '"><span>' + name2 + ' - ' + durationInTraffic.text + textWhenLongerThanUsual + '</span></span>' };
+                                                        if (destinationList[j].match(re3)) { wrapper.innerHTML += '<span class="' + trafficIcon + '"><span>' + name3 + ' - ' + durationInTraffic.text + textWhenLongerThanUsual + '</span></span>' };
+                                                }
+                                        }
+                                }
+                        });
+                }
+                return wrapper;
+        }
 });
